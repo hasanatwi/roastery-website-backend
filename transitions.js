fdb86022b464baacd150c53e36bb9592f5d40372
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2025-12-10 00:44:00 +0200

    Update transitions.js

--- transitions.js
+++ transitions.js
@@ -107,6 +107,7 @@ app.get("/debug", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
